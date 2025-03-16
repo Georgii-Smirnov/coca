c13d105936d238b644e7c332d1be7072cae95ef0
Georgii-Smirnov <shalterplay@gmail.com>
2025-03-16 16:36:55 +0400

    Created header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
-import '../assets/scss/main.scss'
+import "../assets/scss/main.scss";
+
+// component
+
+import { useTheme } from "./components/theme.js";
+import { useBurger } from "./components/burger.js";
 
 
-
-
+useTheme();
+useBurger();
